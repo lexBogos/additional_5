379244468c8abc9b25d16b1d0daa2f04263ab913
lexBogos <lex.bogos@gmail.com>
2018-09-23 14:48:55 +0300

    solution_additional_5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+var decision = true
+var count = 0
+var mystr = 0
+var a = 0
+var b  = 0
+var c = 0
+
+var new_str = str
+while (str.length>0)
+
+	{
+for (q=0; q<bracketsConfig.length; q++)
+	{
+		bracketsConfig[q]
+			
+	c = bracketsConfig[q][0] + bracketsConfig[q][1] 
+
+
+	for (i=0; i < str.length-1; i++ )
+		{
+		a = str[count]
+		b = str[count+1]
+		count = count + 1 
+
+		if ((a+b) == c)
+			{	
+				str = str.substring(0,count-1)+str.substring(count+1)
+			}
+
+		}
+
+	count = 0
+
+	}
+
+	if (str.length==mystr)
+		{if (str.length > 0)
+			{decision = false
+			break}
+		else{decision = true
+			break}	
+		}
+	mystr=str.length
+	}
+  return decision
 }
+
+
